5bab1007e6b98d7bdbd6c5debe54b271a8e49d0a
sina99 <sina.ansari.901@gmail.com>
2025-08-04 14:50:13 -0700

    create form for add room

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Models\User;
 
+use App\Models\Room;
+
 use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
@@ -40,6 +42,41 @@ class AdminController extends Controller
     public function create_room(){
 
         return view('admin.create_room');
+    }
+
+    public function add_room(Request $request){
+
+        $data= new Room;
+
+        $data->room_title=$request->title;
+
+        $data->description=$request->description;
+
+        $data->price=$request->price;
+
+        $data->wifi=$request->wifi;
+
+        $data->room_type=$request->type;
+
+        $image=$request->image;
+
+        if($image){
+
+            $imagename=time().'.'.$image->getClientOriginalExtension();
+
+            $request->image->move('room',$imagename);
+
+            $data->image=$imagename;
+
+        }
+
+        $data->save();
+
+        return redirect()->back();
+
+
+
+
     }
     
 }
--- resources/views/admin/create_room.blade.php
+++ resources/views/admin/create_room.blade.php
@@ -2,6 +2,21 @@
 <html>
   <head> 
     @include('admin.css')
+    <style type="text/css">
+      label
+      {
+        display: inline-block;
+        width: 200 px;
+      }
+      .div_deg{
+        padding-top: 30px;
+      }
+      .div_center{
+        text-align: center;
+        padding-top: 40px;
+      }
+
+    </style>
   </head>
   <body>
     @include('admin.header')
@@ -9,6 +24,55 @@
     <div class="page-content">
         <div class="page-header">
           <div class="container-fluid">
+            <div class="div_center">
+              <h1 style="font-size:30px; font-weight:bold;">Add Rooms</h1>
+              <form action="{{url('add_room')}}" method="Post" enctype="multipart/form-data">
+                @csrf
+                <div class="div_deg">
+                  <label>Room title</label>
+                  <input type='text' name="title">
+                </div>
+                <div class="div_deg">
+                  <label>Description</label>
+                 <textarea name="description"></textarea>
+                </div>
+                <div class="div_deg">
+                  <label>Price</label>
+                  <input type='number' name="price">
+                </div>
+                <div class="div_deg">
+                  <label>Room Type</label>
+                  <select name="type">
+                    <option selected value="regular">Regular</option>
+                    <option value="premium">Premium</option>
+                    <option value="deluxe">Deluxe</option>
+                  </select>
+                </div>
+                <div class="div_deg">
+                  <label>Wifi</label>
+                  <select name="wifi">
+                    <option selected value="yes">Yes</option>
+                    <option value="no">No</option>
+                    
+                  </select>
+                </div>
+                <div class="div_deg">
+                  <label>Upload Image</label>
+                  <input type='file' name="image">
+                </div>
+                <div class="div_deg">
+                  <input class="btn btn-primary" type='submit' value="Add Room">
+                </div>
+              </form>
+
+
+            </div>
+
+
+
+
+
+
           </div>
         </div>
     </div>
